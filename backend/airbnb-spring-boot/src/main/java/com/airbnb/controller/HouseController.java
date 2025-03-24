192e6ba6ee5312770011f8462de0bd12c141228f
Euler Ostos <eulerdavidos@gmail.com>
2025-03-23 21:10:16 -0500

    build(backend): define house endpoints
    The following endpoints are defined:
    - GET api/houses
    - GET api/houses/{id}
    - POST api/houses
    - PUT api/houses/{id}
    - DELETE api/houses/{id}

--- backend/airbnb-spring-boot/src/main/java/com/airbnb/controller/HouseController.java
+++ backend/airbnb-spring-boot/src/main/java/com/airbnb/controller/HouseController.java
@@ -1,13 +1,44 @@
 package com.airbnb.controller;
 
+import com.airbnb.model.House;
 import com.airbnb.service.HouseService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/houses")
 public class HouseController {
     private final HouseService houseService;
 
     public HouseController(HouseService houseService) {
         this.houseService = houseService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<House>> getAllHouses() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<House> getHouseById(@PathVariable Long id) {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<House> createHouse(House house) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<House> updateHouse(@PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteHouse(@PathVariable Long id) {
+        return null;
+    }
+
 }
